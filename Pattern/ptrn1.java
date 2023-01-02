b3ba3b7201b4590a7f4b75a80ce04a17b9f2dc98
anindita-s <110987573+anindita-s@users.noreply.github.com>
2023-01-02 23:19:44 +0530

    Create ptrn1.java

---
+++ Pattern/ptrn1.java
@@ -0,0 +1,15 @@
+class ptrn1
+{
+    public static void main(String args[])
+    {
+        int i,j,n=8;
+		    for(i=0;i<n;i++)
+		    {
+			      for (j=0;j<=i;j++)
+			      {
+				        System.out.print("#");
+			      }
+		        System.out.println();
+        }
+    }
+}
